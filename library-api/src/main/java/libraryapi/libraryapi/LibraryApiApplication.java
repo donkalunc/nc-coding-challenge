03bd3a238d738cbea87eda55b0af367b108c0879
donkalunc <100574451+donkalunc@users.noreply.github.com>
2022-02-28 12:14:48 +0000

    Updated to main

--- library-api/src/main/java/libraryapi/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/libraryapi/libraryapi/LibraryApiApplication.java
@@ -1,13 +1,13 @@
-package com.initalproject.springbootproject;
+package libraryapi.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProjectApplication {
+public class LibraryApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProjectApplication.class, args);
+		SpringApplication.run(LibraryApiApplication.class, args);
 	}
 
 }
--- library-api/src/test/java/libraryapi/libraryapi/LibraryApiApplicationTests.java
+++ library-api/src/test/java/libraryapi/libraryapi/LibraryApiApplicationTests.java
@@ -1,14 +1,13 @@
-package com.initalproject.springbootproject;
+package libraryapi.libraryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootProjectApplicationTests {
+class LibraryApiApplicationTests {
 
 	@Test
 	void contextLoads() {
-
 	}
 
 }
